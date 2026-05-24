dce9655a5d89a94049d8d0f5c47f8775f65f56ea
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-24 11:16:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
-window.onload = () => {
+let progress = 0;
+
+const loader = setInterval(() => {
+
+  progress += 10;
+
+  document.getElementById("loadText").innerText = progress + "%";
+
+  if (progress >= 100) {
+
+    clearInterval(loader);
+
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("screen").style.display = "block";
+
+    startCamera();
+
+  }
+
+}, 200);
+
+
+function startCamera() {
 
   const cam = document.getElementById("cam");
   const status = document.getElementById("status");
 
-  let instability = 0;
-
-  // загрузка
-  let progress = 0;
-  const loader = setInterval(() => {
-
-    progress += Math.floor(Math.random() * 10);
-    document.getElementById("loadText").innerText = progress + "%";
-
-    if (progress >= 100) {
-      clearInterval(loader);
-
-      document.getElementById("loading").style.display = "none";
-      document.getElementById("screen").style.display = "block";
-    }
-
-  }, 150);
-
-  // камера
-  function systemLoop() {
+  setInterval(() => {
 
     let r = Math.random();
 
-    if (r < 0.4) {
+    if (r < 0.5) {
       cam.src = "images/cam_idle.gif";
       status.innerText = "SYSTEM: STABLE";
     }
 
-    else if (r < 0.7) {
-      cam.src = "images/cam_glitch.gif";
-      status.innerText = "WARNING: GLITCH";
-      instability++;
-    }
-
     else {
-      cam.src = "images/cam_alert.gif";
-      status.innerText = "ALERT: ENTITY";
-      instability += 2;
+      cam.src = "images/cam_glitch.gif";
+      status.innerText = "SYSTEM: GLITCH DETECTED";
     }
 
-    if (instability > 6) {
-      cam.src = "images/cam_secret.gif";
-      status.innerText = "HIDDEN FEED";
-      instability = 0;
-    }
-  }
+  }, 3000);
 
-  setInterval(systemLoop, 3000);
-
-};
+}
