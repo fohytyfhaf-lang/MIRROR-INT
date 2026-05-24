f7db34211f1b95248ce6a1ae1c31bec76beacc7e
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-24 11:14:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,55 @@
-const cam = document.getElementById("cam");
-const status = document.getElementById("status");
+window.onload = () => {
 
-let instability = 0;
+  const cam = document.getElementById("cam");
+  const status = document.getElementById("status");
 
-/* ⚡ LOADING SYSTEM */
-let progress = 0;
-const loader = setInterval(() => {
+  let instability = 0;
 
-  progress += Math.floor(Math.random() * 12);
-  document.getElementById("loadText").innerText = progress + "%";
+  // загрузка
+  let progress = 0;
+  const loader = setInterval(() => {
 
-  if (progress >= 100) {
-    clearInterval(loader);
+    progress += Math.floor(Math.random() * 10);
+    document.getElementById("loadText").innerText = progress + "%";
 
-    setTimeout(() => {
+    if (progress >= 100) {
+      clearInterval(loader);
+
       document.getElementById("loading").style.display = "none";
       document.getElementById("screen").style.display = "block";
-    }, 500);
+    }
+
+  }, 150);
+
+  // камера
+  function systemLoop() {
+
+    let r = Math.random();
+
+    if (r < 0.4) {
+      cam.src = "images/cam_idle.gif";
+      status.innerText = "SYSTEM: STABLE";
+    }
+
+    else if (r < 0.7) {
+      cam.src = "images/cam_glitch.gif";
+      status.innerText = "WARNING: GLITCH";
+      instability++;
+    }
+
+    else {
+      cam.src = "images/cam_alert.gif";
+      status.innerText = "ALERT: ENTITY";
+      instability += 2;
+    }
+
+    if (instability > 6) {
+      cam.src = "images/cam_secret.gif";
+      status.innerText = "HIDDEN FEED";
+      instability = 0;
+    }
   }
 
-}, 150);
+  setInterval(systemLoop, 3000);
 
-/* 👁 CAMERA SYSTEM */
-function systemLoop() {
-
-  let r = Math.random();
-
-  if (r < 0.4) {
-    cam.src = "images/cam_idle.gif";
-    status.innerText = "SYSTEM: STABLE FEED";
-  }
-
-  else if (r < 0.7) {
-    cam.src = "images/cam_glitch.gif";
-    status.innerText = "WARNING: SIGNAL INSTABILITY";
-    instability++;
-  }
-
-  else {
-    cam.src = "images/cam_alert.gif";
-    status.innerText = "ALERT: UNKNOWN PRESENCE DETECTED";
-    instability += 2;
-  }
-
-  if (instability > 6) {
-    cam.src = "images/cam_secret.gif";
-    status.innerText = ">>> HIDDEN FEED UNLOCKED <<<";
-    instability = 0;
-  }
-}
-
-setInterval(systemLoop, 3000);
-
-/* 🎮 MANUAL SCAN */
-function scan() {
-  cam.src = "images/cam_glitch.gif";
-  status.innerText = "MANUAL SCAN ACTIVE";
-  instability++;
-}
-
-/* ⚠ FORCE GLITCH */
-function forceGlitch() {
-  cam.src = "images/cam_alert.gif";
-  status.innerText = "SYSTEM OVERLOAD";
-  instability += 3;
-}
+};
