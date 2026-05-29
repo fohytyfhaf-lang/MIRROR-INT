8c8f1e2fd6619be16e9ea1d4efe989a9c0c9a5f3
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 17:53:59 +0300

    Update windows.js

--- systems/windows.js
+++ systems/windows.js
@@ -1,15 +1,41 @@
+const $ = (id) => document.getElementById(id);
+
+function show(id) {
+
+  const el = $(id);
+
+  if (el)
+    el.style.display = "block";
+}
+
+function hide(id) {
+
+  const el = $(id);
+
+  if (el)
+    el.style.display = "none";
+}
+
 function openWindow(id) {
 
-  const win = document.getElementById(id);
+  const win = $(id);
 
   if (!win) return;
 
   win.style.display = "block";
+
+  if (!win.dataset.init) {
+
+    win.style.top = "120px";
+    win.style.left = "120px";
+
+    win.dataset.init = true;
+  }
 }
 
 function closeWindow(id) {
 
-  const win = document.getElementById(id);
+  const win = $(id);
 
   if (!win) return;
 
