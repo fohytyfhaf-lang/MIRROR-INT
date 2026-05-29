4f8173527f3e0422d064c746b6b716b2306418cf
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:44:03 +0300

    Update login.js

--- systems/login.js
+++ systems/login.js
@@ -1,17 +1,17 @@
 function login(){
   const u = $("user").value;
   const p = $("pass").value;
+
   const st = $("loginStatus");
 
-  if(
-    (u==="admin" && p==="1234") ||
-    (u==="operator" && p==="0404")
-  ){
+  if((u==="admin" && p==="1234") || (u==="op" && p==="0404")){
     st.innerText = "ACCESS GRANTED";
+
     setTimeout(()=>{
       hide("login");
       show("desktop");
     },500);
+
   } else {
     st.innerText = "DENIED";
   }
