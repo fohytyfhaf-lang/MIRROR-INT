fbdaae6ab760fac082812854ba08c6fa056d5f47
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:22:08 +0300

    Update game.js

--- systems/game.js
+++ systems/game.js
@@ -1,33 +1,23 @@
-
 function startGame(){
+  const canvas = $("gameCanvas");
+  const ctx = canvas.getContext("2d");
 
-const canvas = $("gameCanvas");
-const ctx = canvas.getContext("2d");
+  let x = 50;
+  let speed = 3;
 
-let x = 50;
-let speed = 3;
+  function loop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 
-function gameLoop(){
+    ctx.fillStyle = "#00ff99";
+    ctx.fillRect(x,100,40,40);
 
-ctx.clearRect(0,0,canvas.width,canvas.height);
+    x += speed;
 
-ctx.fillStyle = "#00ff99";
+    if(x > canvas.width-40 || x < 0)
+      speed *= -1;
 
-ctx.fillRect(x,100,40,40);
+    requestAnimationFrame(loop);
+  }
 
-x += speed;
-
-if(x > canvas.width - 40 || x < 0){
-
-speed *= -1;
-
+  loop();
 }
-
-requestAnimationFrame(gameLoop);
-
-}
-
-gameLoop();
-
-}
-
