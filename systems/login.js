ddd1189a36a2c407e1bd3ee6bc87f56b57b77b09
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 21:37:37 +0300

    Update login.js

--- systems/login.js
+++ systems/login.js
@@ -1,14 +1,23 @@
+export function initLogin() {}
+
 export function loginSystem() {
   const u = document.getElementById("user").value;
   const p = document.getElementById("pass").value;
+
+  const status = document.getElementById("loginStatus");
 
   if (
     (u === "operator" && p === "0404") ||
     (u === "omega" && p === "mirror")
   ) {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("screen").style.display = "block";
+    status.innerText = "ACCESS GRANTED";
+
+    setTimeout(() => {
+      document.getElementById("login").style.display = "none";
+      document.getElementById("screen").classList.remove("hidden");
+    }, 500);
+
   } else {
-    document.getElementById("loginStatus").innerText = "ACCESS DENIED";
+    status.innerText = "ACCESS DENIED";
   }
 }
