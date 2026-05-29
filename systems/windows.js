d41c86a7fbba7f3c6a19e81dff5bb88fcaefbf67
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 17:38:38 +0300

    Update windows.js

--- systems/windows.js
+++ systems/windows.js
@@ -1,1 +1,17 @@
+function openWindow(id) {
 
+  const win = document.getElementById(id);
+
+  if (!win) return;
+
+  win.style.display = "block";
+}
+
+function closeWindow(id) {
+
+  const win = document.getElementById(id);
+
+  if (!win) return;
+
+  win.style.display = "none";
+}
