49072faefb2920fb03bb4d02e5b2a973d988ba6b
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 17:58:07 +0300

    Update camera.js

--- systems/camera.js
+++ systems/camera.js
@@ -1,45 +1,17 @@
-
 let cameraIndex = 0;
 
 const cameras = [
+
   {
     name: "CAM SERVER",
     src: "images/cam_server.gif"
   },
 
   {
-    name: "CAM STORAGE",
-    src: "images/cam_storage.gif"
-  },
-
-  {
     name: "CAM LAB",
     src: "images/cam_lab.gif"
-  },
-
-  {
-    name: "CAM OFFICE",
-    src: "images/cam_office.gif"
-  },
-
-  {
-    name: "CAM HALL",
-    src: "images/cam_hall.gif"
-  },
-
-  {
-    name: "CAM SECRET",
-    src: "images/cam_secret.gif"
   }
 ];
-
-// preload GIF
-cameras.forEach(cam => {
-
-  const img = new Image();
-
-  img.src = cam.src;
-});
 
 function switchCamera(dir) {
 
@@ -51,17 +23,11 @@ function switchCamera(dir) {
   if (cameraIndex >= cameras.length)
     cameraIndex = 0;
 
-  const cam =
-    document.getElementById("cam");
+  const cam = $("cam");
 
-  const name =
-    document.getElementById("cameraName");
+  const name = $("cameraName");
 
-  if (cam)
-    cam.src = cameras[cameraIndex].src;
+  cam.src = cameras[cameraIndex].src;
 
-  if (name)
-    name.innerText = cameras[cameraIndex].name;
+  name.innerText = cameras[cameraIndex].name;
 }
-```
-
