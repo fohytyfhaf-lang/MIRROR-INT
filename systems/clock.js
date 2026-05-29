1fe286fd3dfbe1a68643f65977091b624aaf56f1
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 17:59:09 +0300

    Update clock.js

--- systems/clock.js
+++ systems/clock.js
@@ -1,1 +1,15 @@
+let clockStarted = false;
 
+function startClock() {
+
+  if (clockStarted) return;
+
+  clockStarted = true;
+
+  setInterval(() => {
+
+    $("clock").innerText =
+      new Date().toLocaleTimeString();
+
+  }, 1000);
+}
