8794c8bf80989cc00ce68f322064c0389abecefd
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:21:00 +0300

    Update camera.js

--- systems/camera.js
+++ systems/camera.js
@@ -1,44 +1,21 @@
-
-let cameraIndex = 0;
-
 const cameras = [
-
-{
-name:"CAM SERVER",
-src:"images/cam_server.gif"
-},
-
-{
-name:"CAM STORAGE",
-src:"images/cam_storage.gif"
-},
-
-{
-name:"CAM LAB",
-src:"images/cam_lab.gif"
-},
-
-{
-name:"CAM OFFICE",
-src:"images/cam_office.gif"
-}
-
+  { name:"CAM SERVER", src:"images/cam_server.gif" },
+  { name:"CAM STORAGE", src:"images/cam_storage.gif" },
+  { name:"CAM LAB", src:"images/cam_lab.gif" },
+  { name:"CAM OFFICE", src:"images/cam_office.gif" },
+  { name:"CAM SECRET", src:"images/cam_secret.gif" },
+  { name:"CAM GLITCH", src:"images/cam_glitch.gif" }
 ];
 
 function switchCamera(dir){
+  window.state.cameraIndex += dir;
 
-cameraIndex += dir;
+  if (window.state.cameraIndex < 0)
+    window.state.cameraIndex = cameras.length - 1;
 
-if(cameraIndex < 0)
-cameraIndex = cameras.length - 1;
+  if (window.state.cameraIndex >= cameras.length)
+    window.state.cameraIndex = 0;
 
-if(cameraIndex >= cameras.length)
-cameraIndex = 0;
-
-$("cam").src = cameras[cameraIndex].src;
-
-$("cameraName").innerText =
-cameras[cameraIndex].name;
-
+  $("cam").src = cameras[window.state.cameraIndex].src;
+  $("cameraName").innerText = cameras[window.state.cameraIndex].name;
 }
-
