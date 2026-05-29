3621977c6f368518d6cd746ae83df0b03b1ec0b7
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:33:03 +0300

    Update camera.js

--- systems/camera.js
+++ systems/camera.js
@@ -1,21 +1,13 @@
-const cameras = [
-  { name:"CAM SERVER", src:"images/cam_server.gif" },
-  { name:"CAM STORAGE", src:"images/cam_storage.gif" },
-  { name:"CAM LAB", src:"images/cam_lab.gif" },
-  { name:"CAM OFFICE", src:"images/cam_office.gif" },
-  { name:"CAM SECRET", src:"images/cam_secret.gif" },
-  { name:"CAM GLITCH", src:"images/cam_glitch.gif" }
+let cams = [
+"images/cam_server.gif",
+"images/cam_storage.gif",
+"images/cam_lab.gif"
 ];
 
-function switchCamera(dir){
-  window.state.cameraIndex += dir;
+let i = 0;
 
-  if (window.state.cameraIndex < 0)
-    window.state.cameraIndex = cameras.length - 1;
-
-  if (window.state.cameraIndex >= cameras.length)
-    window.state.cameraIndex = 0;
-
-  $("cam").src = cameras[window.state.cameraIndex].src;
-  $("cameraName").innerText = cameras[window.state.cameraIndex].name;
+function nextCam(){
+  i++;
+  if(i >= cams.length) i = 0;
+  $("cam").src = cams[i];
 }
