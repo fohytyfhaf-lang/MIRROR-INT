59a7b49b369b0d2d56ba19e660cf08393e53a5f1
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:10:22 +0300

    Update core.js

--- systems/core.js
+++ systems/core.js
@@ -1,1 +1,14 @@
 
+const $ = (id) => document.getElementById(id);
+
+function show(id){
+const el = $(id);
+if(el) el.style.display = "block";
+}
+
+function hide(id){
+const el = $(id);
+if(el) el.style.display = "none";
+}
+
+
