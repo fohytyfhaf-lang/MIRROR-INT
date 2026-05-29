e181077ec0c70779fd151d21cb4797f2b13dfbe1
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 17:42:10 +0300

    Update login.js

--- systems/login.js
+++ systems/login.js
@@ -1,1 +1,50 @@
+```js
+function loginSystem() {
+
+  const user =
+    document.getElementById("user")?.value;
+
+  const pass =
+    document.getElementById("pass")?.value;
+
+  const status =
+    document.getElementById("loginStatus");
+
+  let ok = false;
+
+  if (user === "operator" && pass === "0404")
+    ok = true;
+
+  if (user === "research" && pass === "void")
+    ok = true;
+
+  if (user === "omega" && pass === "mirror")
+    ok = true;
+
+  if (!ok) {
+
+    if (status)
+      status.innerText = "ACCESS DENIED";
+
+    return;
+  }
+
+  if (status)
+    status.innerText = "ACCESS GRANTED";
+
+  setTimeout(() => {
+
+    document
+      .getElementById("login")
+      ?.classList.remove("active");
+
+    document
+      .getElementById("screen")
+      .style.display = "block";
+
+    startClock();
+
+  }, 600);
+}
+```
 
