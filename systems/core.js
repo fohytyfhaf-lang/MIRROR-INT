27077832f03f4ed98098fbb532e6f721f107a3a8
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:19:10 +0300

    Update core.js

--- systems/core.js
+++ systems/core.js
@@ -1,14 +1,18 @@
+window.$ = (id) => document.getElementById(id);
 
-const $ = (id) => document.getElementById(id);
+window.show = (id) => {
+  const el = $(id);
+  if (el) el.style.display = "block";
+};
 
-function show(id){
-const el = $(id);
-if(el) el.style.display = "block";
-}
+window.hide = (id) => {
+  const el = $(id);
+  if (el) el.style.display = "none";
+};
 
-function hide(id){
-const el = $(id);
-if(el) el.style.display = "none";
-}
-
-
+window.state = {
+  logged: false,
+  cameraIndex: 0,
+  clockStarted: false,
+  audioStarted: false
+};
