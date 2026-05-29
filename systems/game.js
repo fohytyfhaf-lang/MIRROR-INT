f5243d69e5d1b9a2c9adc20857bcb29c9d73555d
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-29 18:44:59 +0300

    Update game.js

--- systems/game.js
+++ systems/game.js
@@ -1,33 +1,25 @@
-let x = 20;
 let y = 100;
 let vy = 0;
-let gravity = 0.5;
-let jump = false;
 
 function startGame(){
-  const c = $("game");
+  const c = $("canvas");
   const ctx = c.getContext("2d");
 
   document.onkeydown = (e)=>{
-    if(e.code === "Space"){
-      vy = -8;
-    }
+    if(e.code === "Space") vy = -8;
   };
 
   function loop(){
     ctx.fillStyle="black";
     ctx.fillRect(0,0,300,150);
 
-    vy += gravity;
+    vy += 0.5;
     y += vy;
 
-    if(y > 120){
-      y = 120;
-      vy = 0;
-    }
+    if(y > 100){ y = 100; vy = 0; }
 
     ctx.fillStyle="lime";
-    ctx.fillRect(x,y,20,20);
+    ctx.fillRect(50,y,20,20);
 
     requestAnimationFrame(loop);
   }
