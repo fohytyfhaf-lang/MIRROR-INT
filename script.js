bc2b69e27b19bcaa247f63e8cea54b552ad9a8cc
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-31 14:06:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+import { loginSystem } from "./systems/login.js";
+import { openApp, closeApp } from "./systems/windows.js";
+import { sendChat } from "./systems/chat.js";
+import { nextCam } from "./systems/camera.js";
 
+window.login = loginSystem;
+window.openApp = openApp;
+window.closeApp = closeApp;
+window.sendChat = sendChat;
+window.nextCam = nextCam;
