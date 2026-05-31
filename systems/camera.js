a77e4e02e220a3273f1cb0aa3ce809932fc140a6
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-31 14:01:17 +0300

    Update camera.js

--- systems/camera.js
+++ systems/camera.js
@@ -1,16 +1,29 @@
 const cams = [
-  "images/cam_secret.gif",
-  "images/cam_glitch.gif",
-  "images/cam_lab.gif"
+  { src: "images/cam_secret.gif", anomaly: false },
+  { src: "images/cam_lab.gif", anomaly: true },
+  { src: "images/cam_glitch.gif", anomaly: true }
 ];
 
 let i = 0;
 
 export function initCamera() {
-  document.getElementById("camView").src = cams[0];
+  updateCam();
 }
 
 export function nextCam() {
   i = (i + 1) % cams.length;
-  document.getElementById("camView").src = cams[i];
+  updateCam();
+}
+
+function updateCam() {
+
+  const cam = cams[i];
+  const view = document.getElementById("camView");
+
+  view.src = cam.src;
+
+  if (cam.anomaly) {
+    document.getElementById("chatLog").innerText +=
+      "\n⚠ anomaly detected in " + cam.src;
+  }
 }
