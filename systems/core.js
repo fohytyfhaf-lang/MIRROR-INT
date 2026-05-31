1856704176ffb8de70585027c43cbf9da2655e0f
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-31 14:00:04 +0300

    Update core.js

--- systems/core.js
+++ systems/core.js
@@ -3,6 +3,8 @@ import { loginSystem } from "./login.js";
 import { initChat, sendChat } from "./chat.js";
 import { initCamera, nextCam } from "./camera.js";
 import { openApp, closeApp } from "./windows.js";
+import { initLore } from "./lore.js";
+import { MrSmile } from "./mrsmile.js";
 
 window.login = loginSystem;
 window.sendChat = sendChat;
@@ -10,8 +12,19 @@ window.openApp = openApp;
 window.closeApp = closeApp;
 window.nextCam = nextCam;
 
+window.gameState = {
+  time: "night",
+  evidence: [],
+  alertLevel: 0
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+
   initBoot();
   initChat();
   initCamera();
+  initLore();
+
+  MrSmile.init(); // 👁 запускаем скрытую систему
+
 });
