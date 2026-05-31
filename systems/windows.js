062fa8a36dbf4c8f96ff386b114ed3d12a0192d7
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-05-31 13:42:40 +0300

    Update windows.js

--- systems/windows.js
+++ systems/windows.js
@@ -1,3 +1,16 @@
 function openWindow(id){
   show(id);
 }
+const $ = (id) => document.getElementById(id);
+
+export function openApp(name) {
+  const win = $(name + "Window");
+  if (!win) return;
+  win.classList.remove("hidden");
+}
+
+export function closeApp(name) {
+  const win = $(name + "Window");
+  if (!win) return;
+  win.classList.add("hidden");
+}
