0407f8e92d12d4c46078956576aff0272f37171f
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 08:41:35 +0300

    Update clock.js

--- modules/clock.js
+++ modules/clock.js
@@ -1,1 +1,15 @@
+function startClock() {
+  const clock = document.getElementById("clock");
 
+  function update() {
+    const now = new Date();
+    clock.innerText =
+      now.getHours().toString().padStart(2, "0") + ":" +
+      now.getMinutes().toString().padStart(2, "0");
+  }
+
+  update();
+  setInterval(update, 1000);
+}
+
+startClock();
