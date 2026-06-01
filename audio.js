0c310fcd0f62ca0e3c012027bb0713fc52718838
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 14:15:48 +0300

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,23 +1,9 @@
 const bgm = document.getElementById("bgm");
 
-export function playMusic(file, volume = 0.5) {
+export function playMusic(file) {
   if (!bgm) return;
 
-  if (bgm.src.includes(file)) return;
-
   bgm.src = "audio/" + file;
-  bgm.volume = 0;
   bgm.loop = true;
-
   bgm.play().catch(() => {});
-
-  let v = 0;
-  const fade = setInterval(() => {
-    if (v < volume) {
-      v += 0.02;
-      bgm.volume = v;
-    } else {
-      clearInterval(fade);
-    }
-  }, 30);
 }
