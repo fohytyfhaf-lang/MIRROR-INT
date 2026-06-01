0d9de0b70632f3310508a6a0e2c49085f6695742
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 14:13:46 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,16 +1,5 @@
 import { loginSystem } from "./login.js";
-import { openApp, closeApp } from "./windows.js";
 import { playMusic } from "./audio.js";
-import { initMrSmile } from "./mrsmile.js";
 
 window.login = loginSystem;
-window.openApp = openApp;
-window.closeApp = closeApp;
-
-document.addEventListener("DOMContentLoaded", () => {
-  initMrSmile();
-});
-
-window.loginSystem = loginSystem;
-window.openApp = openApp;
-window.closeApp = closeApp;
+window.playMusic = playMusic;
