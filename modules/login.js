3cf760175f051544c21f5dfd9c3c748de65a5955
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 13:52:38 +0300

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -18,16 +18,3 @@ playMusic("background.mp3");
     document.getElementById("status").innerText = "Access Denied";
   }
 }
-
-function login() {
-  console.log("LOGIN START");
-
-  const desktop = document.getElementById("desktop");
-
-  console.log("DESKTOP =", desktop);
-
-  if (!desktop) {
-    alert("desktop is NULL - HTML mismatch or reload issue");
-    return;
-  }
-}
