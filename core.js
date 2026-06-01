71e63174eca3af5e9392130bdbc8b1471272ee21
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 08:27:31 +0300

    Refactor login and window management functions

--- core.js
+++ core.js
@@ -1,12 +1,30 @@
-import { loginSystem } from "./modules/login.js";
-import { openApp, closeApp } from "./modules/windows.js";
-import { sendChat } from "./modules/chat.js";
-import { nextCam } from "./modules/camera.js";
+function login() {
+  const u = document.getElementById("user").value;
+  const p = document.getElementById("pass").value;
 
-window.login = loginSystem;
+  if (u === "operator" && p === "0404") {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("desktop").classList.remove("hidden");
+  } else {
+    document.getElementById("status").innerText = "NO ACCESS";
+  }
+}
+
+function openApp(name) {
+  const w = document.getElementById(name + "Window");
+  if (w) w.classList.remove("hidden");
+}
+
+function closeApp(name) {
+  const w = document.getElementById(name + "Window");
+  if (w) w.classList.add("hidden");
+}
+
+function sendChat() {}
+function nextCam() {}
+
+window.login = login;
 window.openApp = openApp;
 window.closeApp = closeApp;
 window.sendChat = sendChat;
 window.nextCam = nextCam;
-
-console.log("WIN7 ARG SYSTEM ONLINE");
