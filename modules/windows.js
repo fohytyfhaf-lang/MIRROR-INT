33e776499b1a9bf10ef9fb95293a99264f43a508
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 08:20:42 +0300

    Update windows.js

--- modules/windows.js
+++ modules/windows.js
@@ -1,1 +1,9 @@
+export function openApp(name) {
+  const w = document.getElementById(name + "Window");
+  if (w) w.style.display = "block";
+}
 
+export function closeApp(name) {
+  const w = document.getElementById(name + "Window");
+  if (w) w.style.display = "none";
+}
