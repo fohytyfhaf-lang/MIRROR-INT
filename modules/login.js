5ffd9aab458fb67edfd39cb64da9f360dc03aaed
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 08:19:41 +0300

    Update login.js

--- modules/login.js
+++ modules/login.js
@@ -1,1 +1,17 @@
+export function loginSystem() {
+  const u = document.getElementById("user").value;
+  const p = document.getElementById("pass").value;
 
+  if (u === "operator" && p === "0404") {
+
+    document.getElementById("status").innerText = "Welcome";
+
+    setTimeout(() => {
+      document.getElementById("login").classList.add("hidden");
+      document.getElementById("desktop").classList.remove("hidden");
+    }, 500);
+
+  } else {
+    document.getElementById("status").innerText = "Access Denied";
+  }
+}
