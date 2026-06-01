3e52a1b0b4ee633ea270e661a33ab22c8c1252bf
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-01 08:05:12 +0300

    Delete systems directory

--- systems/windows.js
+++
@@ -1,7 +0,0 @@
-export function openApp(name) {
-  document.getElementById(name + "Window").classList.remove("hidden");
-}
-
-export function closeApp(name) {
-  document.getElementById(name + "Window").classList.add("hidden");
-}
