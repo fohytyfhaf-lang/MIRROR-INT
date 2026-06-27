59cf24824920921fd152840b5e5c5fc120b771aa
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-27 14:45:08 +0300

    Update core.js

--- core.js
+++ core.js
@@ -4,6 +4,8 @@ import { runCommand } from "./console.js";
 import { setSoundState } from "./soundManager.js";
 import { listFiles, readFile } from "./filesystem.js";
 import { makeWindowDraggable, bringToFront } from "./windowManager.js";
+import { getSecretFile } from "./secretOrg.js";
+
 /* GLOBAL */
 window.playMusic = playMusic;
 window.setSoundState = setSoundState;
@@ -12,6 +14,7 @@ window.listFiles = listFiles;
 window.readFile = readFile;
 window.makeWindowDraggable = makeWindowDraggable;
 window.bringToFront = bringToFront;
+window.getSecretFile = getSecretFile;
 /* WINDOW SYSTEM */
 function openApp(name) {
   const win = document.getElementById(name + "Window");
