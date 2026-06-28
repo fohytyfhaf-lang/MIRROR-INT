467c33ee87d6bf84f45e4defb64ef509cc64d74c
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-28 11:38:43 +0300

    Update core.js

--- core.js
+++ core.js
@@ -21,6 +21,44 @@ window.nextCam = nextCam;
 
 
 let currentExplorerPath = "/files";
+function renderExplorer(path) {
+  const view = document.getElementById("filesList");
+
+  if (!view) return;
+
+  currentExplorerPath = path;
+
+  const items = listFiles(path);
+
+  view.innerHTML = items.length
+    ? items.map(item => {
+        const fullPath = path + "/" + item;
+
+        return `<div onclick="openExplorerItem('${fullPath}')">
+                  📄 ${item}
+                </div>`;
+      }).join("")
+    : "EMPTY FOLDER";
+}
+window.openExplorerItem = function(path) {
+  const content = readFile(path);
+
+  if (content !== null) {
+    document.getElementById("filesList").innerText = content;
+  } else {
+    renderExplorer(path);
+  }
+};
+window.goBack = function() {
+  if (currentExplorerPath === "/files") return;
+
+  const parts = currentExplorerPath.split("/");
+  parts.pop();
+
+  const newPath = parts.join("/") || "/files";
+
+  renderExplorer(newPath);
+};
 
 /* WINDOW SYSTEM */
 function openApp(name) {
@@ -33,15 +71,7 @@ function openApp(name) {
   }
   
 if (name === "files") {
-  const view = document.querySelector("#filesList");
-
-  if (view) {
-    const files = listFiles("/files");
-
-    view.innerHTML = files.length
-      ? files.map(f => `<div onclick="openFile('/files/${f}')">📄 ${f}</div>`).join("")
-      : "NO FILES FOUND";
-  }
+  renderExplorer("/files");
 }
   
   switch (name) {
@@ -89,16 +119,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
 });
 
-
-window.openFile = function(path) {
-  const content = readFile(path);
-
-  const view = document.querySelector("#filesWindow p");
-
-  if (view) {
-    view.textContent = content;
-  }
-};
 
 /* CLOCK */
 function updateClock() {
