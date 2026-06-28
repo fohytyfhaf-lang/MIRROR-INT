a0efcde723a61996f20dbf79031fd4a874187f0e
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-28 11:50:54 +0300

    Add user role management functions

--- security.js
+++ security.js
@@ -1,1 +1,19 @@
+let userRole = "guest";
 
+const roles = {
+  guest: 0,
+  operator: 1,
+  admin: 2
+};
+
+export function setRole(role) {
+  userRole = role;
+}
+
+export function canAccess(level) {
+  return roles[userRole] >= level;
+}
+
+export function getRole() {
+  return userRole;
+}
