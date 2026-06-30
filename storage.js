2e435e63acdf201862a3c5cbeb9d31e121277085
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-30 10:31:01 +0300

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,14 @@
+export const Storage = {
+  get(key, fallback = null) {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : fallback;
+  },
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  }
+};
