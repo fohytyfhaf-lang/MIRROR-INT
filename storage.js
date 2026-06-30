9a94f01e07231524d9d17aa3578391dcc8882528
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-30 11:47:16 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,5 +1,24 @@
 import { Storage } from "./storage.js";
 
+
+export const Storage = {
+  get(key, fallback = null) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : fallback;
+  },
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  },
+
+  clear() {
+    localStorage.clear();
+  }
+};
 /* =========================
         LANGUAGE PACK
 ========================= */
