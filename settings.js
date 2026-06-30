965f34b61ef89ac3c4af8c455963063e4a71283d
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-06-30 10:34:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 import { Storage } from "./storage.js";
-
+import { setSetting } from "./settings.js";
 export function getSettings() {
   const user = Storage.get("currentUser");
   const users = Storage.get("users", {});
@@ -15,4 +15,38 @@ export function setSetting(key, value) {
 
   users[user].settings[key] = value;
   Storage.set("users", users);
+}
+
+
+
+const slider = document.getElementById("volume");
+
+slider.addEventListener("input", (e) => {
+  const value = Number(e.target.value);
+
+  setSetting("volume", value);
+
+  const audio = document.getElementById("bgm");
+  audio.volume = value;
+});
+
+const langPack = {
+  en: {
+    login: "LOGIN",
+    username: "USERNAME",
+    password: "PASSWORD"
+  },
+  ru: {
+    login: "ВХОД",
+    username: "ИМЯ",
+    password: "ПАРОЛЬ"
+  }
+};
+
+export function setLanguage(lang) {
+  localStorage.setItem("lang", lang);
+
+  document.getElementById("loginBtn").textContent = langPack[lang].login;
+  document.getElementById("user").placeholder = langPack[lang].username;
+  document.getElementById("pass").placeholder = langPack[lang].password;
 }
