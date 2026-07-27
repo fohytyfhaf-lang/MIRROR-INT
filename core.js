fdb1af7fffd4135f45c42b8bfbaa96e2cae8950d
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-07-27 16:40:27 +0300

    Update core.js

--- core.js
+++ core.js
@@ -42,6 +42,7 @@ window.closeApp = closeWindow;
 window.minimizeWindow = minimizeWindow;
 window.maximizeWindow = maximizeWindow;
 window.restoreWindow = restoreWindow;
+window.mrSmileSay = mrSmileSay;
 window.whisper = whisper;
 window.testMrSmile = () => {
     forceEnableMrSmile();
