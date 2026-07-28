b6c3cb95328ed9e8b86b6ece33b40d92650de0df
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-07-28 11:37:39 +0300

    Update core.js

--- core.js
+++ core.js
@@ -14,6 +14,7 @@ import { initMrSmileEvents } from "./mrsmileEvents.js";
 import { forceEnableMrSmile, forceDisableMrSmile } from "./mrsmile.js";
 import { knowledgeInit } from "./knowledge.js";
 import { initSettings} from "./js/settings.js";
+import { SettingsPages } from "/js/settingsPages.js";
 import { Storage } from "./storage.js";
 import { startClock } from "./clock.js";
 import { initPersonnel } from "./personnel.js";
