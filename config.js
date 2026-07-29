517bed55cf14dec3b1a31bc661680fa6202ea838
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-07-29 06:19:27 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,83 @@
+/* ===================================
+            OMEGA CONFIG
+=================================== */
+
+export const CONFIG = {
+
+    /* =========================
+            SYSTEM
+    ========================= */
+
+    NAME: "OMEGA",
+
+    VERSION: "0.2 DEV",
+
+    BUILD: "2026.07",
+
+    DEBUG: true,
+
+    /* =========================
+            LANGUAGE
+    ========================= */
+
+    DEFAULT_LANGUAGE: "en",
+
+    /* =========================
+            AUDIO
+    ========================= */
+
+    DEFAULT_MASTER_VOLUME: 70,
+
+    DEFAULT_MUSIC_VOLUME: 70,
+
+    DEFAULT_EFFECTS_VOLUME: 70,
+
+    /* =========================
+            INTERFACE
+    ========================= */
+
+    DEFAULT_UI_SCALE: 100,
+
+    DEFAULT_FONT_SIZE: 16,
+
+    /* =========================
+            EFFECTS
+    ========================= */
+
+    CRT: true,
+
+    SCANLINES: true,
+
+    ANIMATIONS: true,
+
+    GLITCH_EFFECTS: true,
+
+    /* =========================
+            MR.SMILE
+    ========================= */
+
+    MRSMILE_ENABLED: true,
+
+    MRSMILE_SPAWN_CHANCE: 0.10,
+
+    MRSMILE_DEBUG: false,
+
+    /* =========================
+            GLITCH
+    ========================= */
+
+    RANDOM_GLITCHES: true,
+
+    GLITCH_INTERVAL_MIN: 60000,
+
+    GLITCH_INTERVAL_MAX: 180000,
+
+    /* =========================
+            SECURITY
+    ========================= */
+
+    AUTO_LOGIN: false,
+
+    REMEMBER_USER: false
+
+};
