a06500e7c097c2eb0e8ca4011d50351e76040142
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-07-30 17:54:04 +0300

    Create mrsmileResponses.js

---
+++ mrsmileResponses.js
@@ -0,0 +1,63 @@
+export const Responses = {
+
+    hello: [
+
+        "Hello.",
+        "You're back.",
+        "I knew you would return.",
+        "You came again."
+
+    ],
+
+    help: [
+
+        "Maybe.",
+        "Not today.",
+        "I can help.",
+        "Only if you trust me."
+
+    ],
+
+    omega: [
+
+        "OMEGA lies.",
+        "Read deeper.",
+        "You don't know everything."
+
+    ],
+
+    goodbye: [
+
+        "Stay alive.",
+        "I'll wait.",
+        "Don't disappear."
+
+    ],
+
+    idle: [
+
+        "I am watching.",
+        "Someone is listening.",
+        "The servers never sleep.",
+        "The cameras remember."
+
+    ],
+
+    highTrust: [
+
+        "I'm glad you're here.",
+        "I missed you.",
+        "We can continue."
+
+    ],
+
+    lowTrust: [
+
+        "...",
+        "No.",
+        "Leave.",
+        "I don't trust you."
+
+    ]
+
+};
