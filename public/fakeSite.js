50a4f5860acbeddc8f339b32919afa3e6c93e46f
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-07-31 11:37:46 +0300

    Add fakeSite.js to initialize and load plant data

---
+++ public/fakeSite.js
@@ -0,0 +1,35 @@
+import { plants } from "./fakePlants.js";
+
+export function initFakeSite(){
+
+    loadPlants();
+
+}
+
+function loadPlants(){
+
+    const container = document.getElementById("featuredPlants");
+
+    if(!container) return;
+
+    container.innerHTML="";
+
+    plants.forEach(plant=>{
+
+        container.innerHTML+=`
+
+        <div class="plantCard">
+
+            <h3>${plant.name}</h3>
+
+            <i>${plant.latin}</i>
+
+            <p>${plant.description}</p>
+
+        </div>
+
+        `;
+
+    });
+
+}
