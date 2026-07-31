ca0414ab4fb965ecb14152fc78c2b75718139113
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-07-31 11:38:28 +0300

    Create fakePlants.js with plant data
    Added an array of plant objects with names, Latin names, and descriptions.

---
+++ public/fakePlants.js
@@ -0,0 +1,63 @@
+export const plants = [
+
+{
+
+name:"White Oak",
+
+latin:"Quercus alba",
+
+description:"A large hardwood tree native to eastern North America."
+
+},
+
+{
+
+name:"Douglas Fir",
+
+latin:"Pseudotsuga menziesii",
+
+description:"One of the tallest conifer species."
+
+},
+
+{
+
+name:"Sugar Maple",
+
+latin:"Acer saccharum",
+
+description:"Famous for maple syrup."
+
+},
+
+{
+
+name:"Sunflower",
+
+latin:"Helianthus annuus",
+
+description:"A flowering plant cultivated worldwide."
+
+},
+
+{
+
+name:"Sage",
+
+latin:"Salvia officinalis",
+
+description:"An aromatic medicinal herb."
+
+},
+
+{
+
+name:"Prickly Pear",
+
+latin:"Opuntia",
+
+description:"A cactus native to the Americas."
+
+}
+
+];
