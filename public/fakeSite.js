337edb6bcc8f809bdae68d808c9838d75594454d
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-08-01 14:44:25 +0300

    Implement loadArticles function in fakeSite.js
    Added loadArticles function to display articles on the fake site.

--- public/fakeSite.js
+++ public/fakeSite.js
@@ -1,11 +1,12 @@
 import { plants } from "./fakePlants.js";
 import { news } from "./fakeNews.js";
+import { articles } from "./fakeArticles.js";
 
 export function initFakeSite(){
 
     loadPlants();
     loadNews();
-
+    loadArticles();
 }
 
 function loadPlants(){
@@ -58,6 +59,49 @@ function loadNews(){
         —
         ${item.text}
         </li>
+
+        `;
+
+
+    });
+
+}
+
+function loadArticles(){
+
+    const container =
+    document.getElementById("articleList");
+
+
+    if(!container) return;
+
+
+    container.innerHTML = "";
+
+
+    articles.forEach(article => {
+
+
+        container.innerHTML += `
+
+        <div class="articleCard">
+
+            <h3>
+                ${article.title}
+            </h3>
+
+
+            <small>
+                ${article.date}
+            </small>
+
+
+            <p>
+                ${article.text}
+            </p>
+
+
+        </div>
 
         `;
 
