8172fbb77e26330464c0ed934ffb540e3d3cdf15
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-08-01 14:40:30 +0300

    Create fakeNews.js

---
+++ public/fakeNews.js
@@ -0,0 +1,23 @@
+export const news = [
+
+    {
+        date: "April 12, 2013",
+        text: "Spring Survey Completed"
+    },
+
+    {
+        date: "March 30, 2013",
+        text: "Herbarium Archive Updated"
+    },
+
+    {
+        date: "March 18, 2013",
+        text: "New Species Added"
+    },
+
+    {
+        date: "February 21, 2013",
+        text: "Research Expedition Begins"
+    }
+
+];
