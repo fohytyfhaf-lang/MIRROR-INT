bb2b79df42f0abb2baeef1c87faeb2093a270748
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-08-14 14:46:39 +0300

    Update chats.js

--- chats.js
+++ chats.js
@@ -624,6 +624,24 @@ function sendMessage() {
 
 }
 
+/* =========================================================
+   CURRENT TIME
+========================================================= */
+
+function getCurrentTime() {
+
+    const now =
+        new Date();
+
+    return (
+        String(now.getHours()).padStart(2, "0")
+        +
+        ":"
+        +
+        String(now.getMinutes()).padStart(2, "0")
+    );
+
+}
 
 /* =========================================================
    MR.SMILE BASIC RESPONSE
