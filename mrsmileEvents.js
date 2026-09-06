9cebbd2eb1faee478bc930bab8bdf67603787444
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-09-06 11:44:14 +0300

    Update mrsmileEvents.js

--- mrsmileEvents.js
+++ mrsmileEvents.js
@@ -210,6 +210,30 @@ export function initMrSmileEvents() {
    initMrSmileActions();
    initMrSmileIntrusionUI();
 
+   /* ======================================================
+       TEMP TEST
+    ====================================================== */
+
+    on("mrsmile:testBehavior", () => {
+
+        trigger("mrsmile:actionRequested", {
+            action: "observe",
+            target: "test",
+            reason: "manual_test",
+            timestamp: Date.now(),
+
+            relationship: {
+                trust: 0,
+                respect: 50,
+                irritation: 0,
+                score: 33,
+                level: "cold"
+            }
+        });
+
+    });
+
+
 
     /* ------------------------------------------------------
        FIRST CONTACT
