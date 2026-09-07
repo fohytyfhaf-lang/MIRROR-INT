c33be374457c9ec2e2bcc46a0eba48e1bda9798d
fohytyfhaf-lang <fohytyfhaf@gmail.com>
2026-09-07 11:26:16 +0300

    Update mrsmileChat.js

--- mrsmileChat.js
+++ mrsmileChat.js
@@ -1363,6 +1363,39 @@ window.debugMrSmileChat = {
         );
 
     },
+   
+/* 
+Тест event-driven последовательности.
+*/
+
+   sequenceTest() {
+
+    trigger(
+        "mrsmile:chatSequence",
+        {
+            stopIdle: true,
+
+            messages: [
+                {
+                    text: "You shouldn't be here.",
+                    delay: 500
+                },
+                {
+                    text: "But you already know that.",
+                    delay: 1400
+                },
+                {
+                    text: "Continue.",
+                    delay: 1000
+                }
+            ],
+
+            resumeIdle: true
+        }
+    );
+
+},
+
 
 
     /*
@@ -1414,30 +1447,3 @@ window.debugMrSmileChat = {
     }
 
 };
-sequenceTest() {
-
-    trigger(
-        "mrsmile:chatSequence",
-        {
-            stopIdle: true,
-
-            messages: [
-                {
-                    text: "You shouldn't be here.",
-                    delay: 500
-                },
-                {
-                    text: "But you already know that.",
-                    delay: 1400
-                },
-                {
-                    text: "Continue.",
-                    delay: 1000
-                }
-            ],
-
-            resumeIdle: true
-        }
-    );
-
-},
